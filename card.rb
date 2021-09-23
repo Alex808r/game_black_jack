baa2695a9c55bc89a10636521fe4a2100985314e
Alex808r <fromxlam@gmail.com>
2021-09-23 12:39:41 +0300

    Add class Card

---
+++ card.rb
@@ -0,0 +1,30 @@
+class Card
+
+  attr_accessor :rank, :suit,:score
+
+  CARD_SCORES = {
+    '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7,
+    '8' => 8, '9' => 8, '10' => 10, 'J' => 10, 'Q' => 10, 'K' => 10,
+    'A' => 11
+  }
+
+  CARD_SUITS = %w[крести буби черви вини]
+
+  def initialize(rank, suit, score)
+    @rank = rank
+    @suit = suit
+    @score = score
+  end
+
+  def random_test
+    @rank = CARD_SCORES.to_a.sample[0]
+    @suit = CARD_SUITS.sample
+    @score = CARD_SCORES.to_a.sample[0]
+  end
+end
+
+card = Card.new('1','2','3')
+p card
+
+card.random_test
+p card
