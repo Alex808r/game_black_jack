abdea2a224112fc7039a83e47bd7fb26384d5cd4
Alex808r <fromxlam@gmail.com>
2021-09-23 14:24:11 +0300

    Add class Hand

--- interface.rb
+++ interface.rb
@@ -3,25 +3,8 @@ require_relative 'player'
 class Interface
 
   def initialize
-    @players = []
   end
 
-  def main_menu
-    loop do
-      print "Введите команду=>: "
-      choice = gets.strip.to_i
-      case choice
-      when 0 then break
-      when 1 then create_player
-      end
-    end
-  end
-
-  def create_player
-    print "Введите имя пользователя: "
-    @players.push(Player.new(gets.chomp))
-  end
 
 end
 
-Interface.new.main_menu
--- main.rb
+++ main.rb
@@ -6,8 +6,18 @@ require_relative 'bank'
 class Main
 
   def initialize
-    
+    @bank = Bank.new
+    @player = Player.new('Dealer')
+    @deck = []
+    create_player
+
   end
+
+  def create_player
+    print "Введите имя пользователя: "
+    @player = Player.new(gets.strip.capitalize)
+  end
+
 
 end
 
